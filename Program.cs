075fc493665c5def35e8e7dd5192f30e6f9f09d2
higor <higorbandeira@hotmail.com>
2025-09-14 13:16:48 -0300

    feat: ajustes para executar as transacoes corretamente, respeitando a sequencia de execucao.

--- Program.cs
+++ Program.cs
@@ -12,21 +12,21 @@ namespace TransacaoFinanceira
 
         static void Main(string[] args)
         {
-            Transacao[] TRANSACOES = new Transacao[] { new Transacao (1,"09/09/2023 14:15:00", 938485762, 2147483649, 150),
-                                                        new Transacao (2,"09/09/2023 14:15:05", 2147483649, 210385733, 149),
-                                                        new Transacao (3,"09/09/2023 14:15:29", 347586970, 238596054, 1100),
-                                                        new Transacao (4,"09/09/2023 14:17:00", 675869708, 210385733, 5300),
-                                                        new Transacao (5,"09/09/2023 14:18:00", 238596054, 674038564, 1489),
-                                                        new Transacao (6,"09/09/2023 14:18:20", 573659065, 563856300, 49),
-                                                        new Transacao (7,"09/09/2023 14:19:00", 938485762, 2147483649, 44),
-                                                        new Transacao (8,"09/09/2023 14:19:01", 573659065, 675869708, 150)
+            Transacao[] TRANSACOES = new Transacao[] {  
+                new Transacao (1,"09/09/2023 14:15:00", 938485762, 2147483649, 150),
+                new Transacao (2,"09/09/2023 14:15:05", 2147483649, 210385733, 149),
+                new Transacao (3,"09/09/2023 14:15:29", 347586970, 238596054, 1100),
+                new Transacao (4,"09/09/2023 14:17:00", 675869708, 210385733, 5300),
+                new Transacao (5,"09/09/2023 14:18:00", 238596054, 674038564, 1489),
+                new Transacao (6,"09/09/2023 14:18:20", 573659065, 563856300, 49),
+                new Transacao (7,"09/09/2023 14:19:00", 938485762, 2147483649, 44),
+                new Transacao (8,"09/09/2023 14:19:01", 573659065, 675869708, 150)
             };
             executarTransacaoFinanceira executor = new executarTransacaoFinanceira();
-            Parallel.ForEach(TRANSACOES, item =>
+            foreach (var item in TRANSACOES)
             {
                 executor.transferir(item.CorrelationId, item.ContaOrigem, item.ContaDestino, item.Valor);
-            });
-
+            }
         }
     }
 
@@ -37,7 +37,7 @@ namespace TransacaoFinanceira
             contas_saldo conta_saldo_origem = getSaldo<contas_saldo>(conta_origem) ;
             if (conta_saldo_origem.saldo < valor)
             {
-                Console.WriteLine("Transacao numero {0 } foi cancelada por falta de saldo", correlation_id);
+                Console.WriteLine("Transacao numero {0} foi cancelada por falta de saldo", correlation_id);
 
             }
             else
@@ -45,7 +45,7 @@ namespace TransacaoFinanceira
                 contas_saldo conta_saldo_destino = getSaldo<contas_saldo>(conta_destino);
                 conta_saldo_origem.saldo -= valor;
                 conta_saldo_destino.saldo += valor;
-                Console.WriteLine("Transacao numero {0} foi efetivada com sucesso! Novos saldos: Conta Origem:{1} | Conta Destino: {3}", correlation_id, conta_saldo_origem.saldo, conta_saldo_destino.saldo);
+                Console.WriteLine("Transacao numero {0} foi efetivada com sucesso! Novos saldos: Conta Origem:{1} | Conta Destino: {2}", correlation_id, conta_saldo_origem.saldo, conta_saldo_destino.saldo);
             }
         }
     }
--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Passos a implementar:
     Criaçao de um objeto para normalizar os dados de entrada. Um registro do tipo uint encontrado, normalizei todos os tipo para o mesmo uint.
     
 2. Execute o programa para avaliar a saida, identifique e corrija o motivo de algumas transacoes estarem sendo canceladas mesmo com saldo positivo e outras sem saldo sendo efetivadas.
+    Foi ajustado o indice do texto que informa a efetivação da transação. Também foi removido o foreach paralelo por um sincrono, para evitar erro na sequencia de transações de mesma origem.
+
 3. Aplique o code review e refatore conforme as melhores praticas(SOLID,Patterns,etc).
 4. Implemente os testes unitários que julgar efetivo.
 5. Crie um git hub e compartilhe o link respondendo o ultimo e-mail.
